4042b2bd0e54a93401020ed678ecb433fa58429b
Manoj <manojbilla2005@gmail.com>
2024-08-20 21:57:58 +0530

    changes in speakers, call for papers

--- src/components/call.jsx
+++ src/components/call.jsx
@@ -4,28 +4,71 @@ export default function Call() {
     return (
         <div id='#call-for-papers' className='call-container'>
             <h2>
-                CALL FOR PAPERS
+                GUIDELINES
             </h2>
             <p>
-                Authors are encouraged to submit an electronic version of their original research papers in the
-                prescribed format <a href="https://docs.google.com/document/d/1clbTDLxesEVLAz_Y_L1PXsT94Lq23XzY/edit?usp=sharing&ouid=111684554937704534813&rtpof=true&sd=true" target="_blank"><span className='cal-link'>(IEEE - Format)</span></a>. Please ensure that the content
-                submitted has not been part of any other conference proceedings or journal papers. All papers
-                would be subject to rigorous review. All accepted papers, after the receipt of author registration
-                will appear in full-length in the conference proceedings (soft copy). For all communications and for
-                paper submissions use the e-mail address <span className='cal-link'><a href='mailto:icamm2025@psgtech.ac.in'>icamm2025@psgtech.ac.in</a></span>.
+                <p>1. All submitted papers must be original and should not have been published previously,
+                nor should they be submitted for publication elsewhere before or after the conference.</p>
+                <p>2. Submit your paper in the prescribed format <a href="https://docs.google.com/document/d/1clbTDLxesEVLAz_Y_L1PXsT94Lq23XzY/edit?usp=sharing&ouid=111684554937704534813&rtpof=true&sd=true" target="_blank"><span className='cal-link'>(IEEE - Format)</span></a>. Utilize the sample paper
+                provided on the conference website and carefully review the &quot;Rules and Regulations&quot; of
+                the conference.</p>
+                <p>3. ICAMM 2025 will not publish any paper that has not been registered.</p>
+                <p>4. If a complaint regarding the authorship of a paper is received and found to be valid, the
+                paper will be immediately withdrawn from publication.</p>
+                <p>5. All conference notifications will be communicated through your registered email ID and
+                mobile number.</p>
             </p>
-
+            <br />
+            <h2>
+                PUBLICATION IN INTERNATIONAL/NATIONAL JOURNALS
+            </h2>
+            <p>
+                <p>1. ICAMM 2025 reserves the right to select papers from the conference for publication in
+                international journals.</p>
+                <p>2. Upon a written request from the author, ICAMM 2025 can submit a paper to any
+                international journal indexed in Scopus or SCI. In such cases, the author will be
+                responsible for covering the full publication costs as per the journal&#39;s requirements. The
+                review and publication process may take between 3 to 12 months.</p>
+                <p>3. The ICAMM 2025 review process for associated international journals typically takes a
+                minimum of 30 to 60 days to complete after the conference.</p>
+                <p>4. The international journals associated with ICAMM 2025 are ISSN-approved.</p>
+                <p>5. Authors who wish to receive a hard copy of the journal must cover the associated
+                charges.</p>
+                <p>6. The editorial board holds the right to reject any paper for publication in an international
+                journal after the second round of review (post-conference). If a paper is rejected, the
+                conference registration fee will not be refunded.</p>
+            </p>
+            <br />
+            <h2>
+                PLAGIARISM POLICY
+            </h2>
+            <p>
+                <p>1. Authors are responsible for checking the plagiarism (below 25%) of their papers before
+                submission. Authors who republish their previously published work (self-plagiarism) will
+                be subject to the same procedures as outlined above.</p>
+                <p>2. All research papers submitted for publication undergo plagiarism detection using
+                specialized software to verify their originality and determine the similarity percentage.
+                Papers that do not meet the required criteria will be returned to the author for revision.</p>
+            </p>
+            <br />
             <h2>
                 JOURNAL PUBLICATION
             </h2>
             <p>
-                The proceedings of ICAMM 2025 will be published in soft copy. Selected papers will be
-                published in National Journal of Technology (ISSN Code : 0973-1334) based on suggestions
-                from the reviewers and session chairs. The organizing committee is also in discussions for
-                publication with journals that are listed in the Web of Sciences / Scopus / SCI. Only accepted manuscripts for journal publications are subject
-                to article processing charges.
-            </p>
+            The selected papers will be recommended for publication in one of the following list of journals
+            based on suggestions from the review team. However, the final decision on publication will be
+            made by the editorial team of the concerned journals.
 
+            <ul style={{color:'red'}}>
+                <li>Journal of Fractional Calculus and Non-linear System</li>
+                <li>Journal of Mathematical Analysis and Modeling</li>
+                <li>Journal of Mathematical Control Sciences and Applications</li>
+            </ul> 
+
+            The organizing committee is also in discussions for publication with journals that are listed in
+            the Scopus/SCI. Article processing charges are journal dependent and to be borne by the authors.
+            </p>
+            <br />
             <h2>
                 ACCOMMODATION
             </h2>
@@ -34,7 +77,7 @@ export default function Call() {
                 the college, on a chargeable basis subject to availability. Requests for the same must be made in
                 advance.
             </p>
-
+            <br />
             <h2>
                 VISA
             </h2>
@@ -42,7 +85,7 @@ export default function Call() {
                 All overseas citizens travelling to India require a visa. A general portal with information about
                 consulates and visa services for India is at <a href="http://www.visatoindia.com" target='_blank'><span className='cal-link'>http://www.visatoindia.com</span></a>.
             </p>
-
+            <br />
             <h2>
                 VENUE
             </h2>
