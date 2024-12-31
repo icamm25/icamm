8a738fd1135c4c62472f492b63a9029b90e9d901
Srinivasansoundar <srinisoundar66@gmail.com>
2024-12-31 11:21:40 +0530

    schedule added

--- src/components/dates.jsx
+++ src/components/dates.jsx
@@ -17,35 +17,39 @@ export default function Dates(){
                 <div className="sc_box">
                     <div className="sc_circle">
                         <div className="sc_inside">
-                        <button className='sc_link' onClick={()=>setModal(true)}>Day  1</button>
-                             {modal && ( 
+                        <a href="https://drive.google.com/file/d/1jonpiPWV6vSDOcIPMO72WN2sltEMVorh/view?usp=sharing" target="_blank" className='sc_link' >Day  1
+                           {/* onClick={()=>setModal(true)} */}
+                             {/* {modal && ( 
                                 <Modal title={<span>Day 1</span>}
                                 content={<h3>Will be updated soon... </h3>}
                                 close={setModal}
                                 />
-                             )}    
+                             )}     */}
+                        </a>
                         </div>
                     </div>
                     <div className="sc_circle">
                         <div className="sc_inside">
-                        <button className='sc_link' onClick={()=>setModal1(true)}>Day 2</button>
-                             {modal1 && ( 
+                        <a href="https://drive.google.com/file/d/12K978dzm5tUNuXCA3HOph90-XiPtT0is/view?usp=sharing" target="_blank" className='sc_link' >Day 2
+                          {/* onClick={()=>setModal1(true)} */}
+                             {/* {modal1 && ( 
                                 <Modal title={<span>Day 2</span>}
                                 content={<h3>Will be updated soon... </h3>}
                                 close={setModal1}
                                 />
-                             )}   
+                             )}    */}
+                             </a>
                         </div>
                     </div>
                     <div className="sc_circle">
                         <div className="sc_inside">
-                        <button className='sc_link' onClick={()=>setModal2(true)}>Day 3</button>
-                             {modal2 && ( 
+                        <a href="https://drive.google.com/file/d/1Rtzt_A1XF3oJGCn0pxSYb_1U53n_sTCc/view?usp=sharing" target ="_blank" className='sc_link' >Day 3</a>
+                             {/* {modal2 && ( 
                                 <Modal title={<span>Day 3</span>}
                                 content={<h3>Will be updated soon... </h3>}
                                 close={setModal2}
                                 />
-                             )}
+                             )} */}
                         </div>
                     </div>
                 </div>
